501920c6f998876500cedbaee28154a356cae12e
Lucas <lucasphone75@gmail.com>
2026-03-09 20:05:36 -0600

    debugs

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -36,7 +36,7 @@ constexpr size_t HUGE_PAGE_SIZE = 2 * 1024 * 1024;
 
 // ─── Benchmark constants ───
 
-constexpr size_t NUM_OPS = 100000;
+constexpr size_t NUM_OPS = 50000;
 constexpr size_t NUM_CLIENTS = 1;
 constexpr size_t NUM_OPS_PER_CLIENT = NUM_OPS / NUM_CLIENTS;
 constexpr size_t NUM_TOTAL_OPS = NUM_OPS_PER_CLIENT * NUM_CLIENTS;
