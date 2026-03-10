b11fb561c78a6079b7c28ccadb6226c9799df08e
Lucas <lucasphone75@gmail.com>
2026-03-09 21:53:05 -0600

    optimize

--- src/servers/mu_follower.cpp
+++ src/servers/mu_follower.cpp
@@ -95,11 +95,19 @@ void MuFollower::run() {
     uint64_t applied[MAX_LOCKS] = {};
     auto* local_buf = static_cast<uint8_t*>(buf_);
 
-    // test: read just the first header
-    volatile uint64_t* p = reinterpret_cast<volatile uint64_t*>(local_buf);
-    uint64_t val = *p;
-    std::cout << "[MuFollower] lock 0 header = " << val << "\n";
-    std::cout.flush();
+    volatile uint64_t* commit_ptrs[MAX_LOCKS];
+    for (uint32_t i = 0; i < MAX_LOCKS; ++i) {
+        commit_ptrs[i] = reinterpret_cast<volatile uint64_t*>(
+            local_buf + i * LOCK_REGION_SIZE);
+    }
 
-    while (true) {}
+    while (true) {
+        for (uint32_t lock_id = 0; lock_id < MAX_LOCKS; ++lock_id) {
+            const uint64_t committed = *commit_ptrs[lock_id];
+
+            while (applied[lock_id] < committed) {
+                applied[lock_id]++;
+            }
+        }
+    }
 }
