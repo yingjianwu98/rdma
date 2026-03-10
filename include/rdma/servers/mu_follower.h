fa79a682a8477fa36ab4eb18947ceba7ac883cb7
Lucas <lucasphone75@gmail.com>
2026-03-09 22:38:32 -0600

    try client to client connections

--- include/rdma/servers/mu_follower.h
+++ include/rdma/servers/mu_follower.h
@@ -6,6 +6,6 @@ class MuFollower : public Server {
 public:
     explicit MuFollower(const uint32_t node_id) : Server(node_id) {}
 protected:
-    [[nodiscard]] uint32_t expected_clients() const override { return 0; }
+    [[nodiscard]] uint32_t expected_clients() const override { return NUM_CLIENTS; }
     void run() override;
 };
