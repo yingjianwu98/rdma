2f257e19c351d3f34b6909a3970f3eeb196f71e2
Lucas <lucasphone75@gmail.com>
2026-03-13 00:41:19 -0600

    read first before spinning

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -44,7 +44,7 @@ constexpr size_t MAX_REPLICAS = 10;
 
 // ─── Benchmark constants ───
 
-constexpr size_t NUM_OPS = 2000000;
+constexpr size_t NUM_OPS = 5000000;
 constexpr size_t NUM_CLIENTS_PER_MACHINE = 16;
 constexpr size_t TOTAL_MACHINES = 5;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
