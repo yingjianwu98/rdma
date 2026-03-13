36aaf92d3e12550f53f1c4c25033e78e21e8ebbb
Lucas <lucasphone75@gmail.com>
2026-03-13 01:08:52 -0600

    add debugs

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -44,7 +44,7 @@ constexpr size_t MAX_REPLICAS = 10;
 
 // ─── Benchmark constants ───
 
-constexpr size_t NUM_OPS = 1000000;
+constexpr size_t NUM_OPS = 3000000;
 constexpr size_t NUM_CLIENTS_PER_MACHINE = 8;
 constexpr size_t TOTAL_MACHINES = 7;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
