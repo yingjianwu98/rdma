b14496e59ff9fd65648816e1f7d8731fb9539dc0
Lucas <lucasphone75@gmail.com>
2026-03-12 19:56:04 -0600

    share event

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -45,7 +45,7 @@ constexpr size_t MAX_REPLICAS = 10;
 // ─── Benchmark constants ───
 
 constexpr size_t NUM_OPS = 5000000;
-constexpr size_t NUM_CLIENTS_PER_MACHINE = 16;
+constexpr size_t NUM_CLIENTS_PER_MACHINE = 32;
 constexpr size_t TOTAL_MACHINES = 7;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
 constexpr size_t NUM_OPS_PER_CLIENT = NUM_OPS / TOTAL_CLIENTS;
