34adf2d61bb8905464269b0484af70065d7692a4
Lucas <lucasphone75@gmail.com>
2026-03-14 21:43:06 -0600

    add replication to cas pipeline

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -48,7 +48,7 @@ constexpr uint8_t RDMA_INITIATOR_DEPTH = 16;
 // ─── Benchmark constants ───
 
 constexpr size_t NUM_OPS = 20000000;
-constexpr size_t NUM_CLIENTS_PER_MACHINE = 4;
+constexpr size_t NUM_CLIENTS_PER_MACHINE = 1;
 constexpr size_t TOTAL_MACHINES = 1;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
 constexpr size_t NUM_OPS_PER_CLIENT = NUM_OPS / TOTAL_CLIENTS;
@@ -56,7 +56,7 @@ constexpr size_t NUM_TOTAL_OPS = NUM_OPS_PER_CLIENT * TOTAL_CLIENTS;
 
 // ─── Lock table layout ───
 
-constexpr size_t MAX_LOCKS = 1000;
+constexpr size_t MAX_LOCKS = 10000;
 constexpr size_t CAS_ACTIVE_CLIENTS = 16;
 constexpr size_t MAX_LOG_PER_LOCK = ((NUM_OPS + MAX_LOCKS - 1) / MAX_LOCKS) * 4;
 constexpr size_t LOCK_HEADER_SIZE = 8;
