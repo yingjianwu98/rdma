99ce69a8a1f294d2f083c820f326c5b3eff94332
Lucas <lucasphone75@gmail.com>
2026-03-14 18:48:33 -0600

    swap configuration

--- src/cas_pipeline.cpp
+++ src/cas_pipeline.cpp
@@ -221,7 +221,7 @@ void post_release(Client& client, CasOpCtx& op) {
 
 CasPipelineConfig load_cas_pipeline_config() {
     CasPipelineConfig config{};
-    config.active_window = std::max<size_t>(1, get_uint_env_or("CAS_ACTIVE_WINDOW", 1));
+    config.active_window = std::max<size_t>(1, get_uint_env_or("CAS_ACTIVE_WINDOW", 16));
     config.cq_batch = std::max<size_t>(1, get_uint_env_or("CAS_CQ_BATCH", 32));
     config.zipf_skew = get_double_env_or("CAS_ZIPF_SKEW", 0.99);
     return config;
