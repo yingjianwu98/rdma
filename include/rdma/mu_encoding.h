0b416b089ed267956a79d33cb98827c7be481c3a
Lucas <lucasphone75@gmail.com>
2026-03-15 01:15:56 -0600

    add mu pipeline

--- include/rdma/mu_encoding.h
+++ include/rdma/mu_encoding.h
@@ -101,7 +101,7 @@ inline void mu_write_entry_word(uint8_t* lock_base, const uint32_t slot, const u
 constexpr size_t MU_CLIENT_SEND_SIGNAL_EVERY_DEFAULT = 64;
 constexpr size_t MU_SERVER_SEND_SIGNAL_EVERY_DEFAULT = 128;
 constexpr size_t MU_CLIENT_CQ_BATCH_DEFAULT = 32;
-constexpr size_t MU_SERVER_RECV_RING = 256;
+constexpr size_t MU_SERVER_RECV_RING = 512;
 constexpr size_t MU_MAX_PENDING_PER_LOCK = 1024;
-constexpr size_t MU_MAX_APPEND_INFLIGHT_PER_LOCK = 16;
+constexpr size_t MU_MAX_APPEND_INFLIGHT_PER_LOCK = 32;
 constexpr size_t MU_REPL_SIGNAL_QUORUM_ONLY_DEFAULT = 1;
