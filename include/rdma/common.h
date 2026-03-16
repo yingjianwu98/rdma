b5b35b601ec0a9417c18de23e399881bce47ddf4
Lucas <lucasphone75@gmail.com>
2026-03-15 23:25:26 -0600

    add faa pipeline

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -58,7 +58,7 @@ constexpr size_t NUM_TOTAL_OPS = NUM_OPS_PER_CLIENT * TOTAL_CLIENTS;
 // ─── Lock table layout ───
 
 constexpr size_t MAX_LOCKS = 1000;
-constexpr size_t FAA_ACTIVE_WINDOW = 16;
+constexpr size_t FAA_ACTIVE_WINDOW = 64;
 constexpr size_t MU_ACTIVE_WINDOW = 32;
 constexpr size_t TAS_ACTIVE_WINDOW = 32;
 constexpr size_t CAS_ACTIVE_CLIENTS = 32;
