d6913d9859e06d654ea1352e42345a9bf11c1fba
Lucas <lucasphone75@gmail.com>
2026-03-17 21:50:16 -0600

    refactor and inline cas

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -55,7 +55,7 @@ constexpr size_t TOTAL_MACHINES = 1;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
 constexpr size_t NUM_OPS_PER_CLIENT = NUM_OPS / TOTAL_CLIENTS;
 constexpr size_t NUM_TOTAL_OPS = NUM_OPS_PER_CLIENT * TOTAL_CLIENTS;
-constexpr size_t MAX_LOCKS = 1000;
+constexpr size_t MAX_LOCKS = 10000;
 
 // ─── CAS config ───
 
