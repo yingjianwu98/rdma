a0391bd938c44fab05b76093bfc110c7a6bf389e
Lucas <lucasphone75@gmail.com>
2026-03-17 23:10:56 -0600

    refactor and inline cas

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -49,7 +49,7 @@ constexpr uint8_t RDMA_INITIATOR_DEPTH = 16;
 
 // ─── Benchmark / workload config ───
 
-constexpr size_t NUM_OPS = 500;
+constexpr size_t NUM_OPS = 500000;
 constexpr size_t NUM_CLIENTS_PER_MACHINE = 1;
 constexpr size_t TOTAL_MACHINES = 1;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
