feacc539719862970c7e9cc700a7c29e5ab65fc8
Lucas <lucasphone75@gmail.com>
2026-03-19 16:21:46 -0600

    edit config

--- include/rdma/common.h
+++ include/rdma/common.h
@@ -33,7 +33,7 @@ inline const std::vector<std::string> CLIENT_NODES = {
      // "192.168.1.4",
    // "192.168.1.5",
     "192.168.1.6",
-    // "192.168.1.7",
+    "192.168.1.7",
     // "192.168.1.8",
     // "192.168.1.9",
     // "192.168.1.10",
@@ -56,7 +56,7 @@ constexpr uint8_t RDMA_INITIATOR_DEPTH = 16;
 
 constexpr size_t NUM_OPS = 15000000;
 constexpr size_t NUM_CLIENTS_PER_MACHINE = 16;
-constexpr size_t TOTAL_MACHINES = 1;
+constexpr size_t TOTAL_MACHINES = 2;
 constexpr size_t TOTAL_CLIENTS = NUM_CLIENTS_PER_MACHINE * TOTAL_MACHINES;
 constexpr size_t NUM_OPS_PER_CLIENT = NUM_OPS / TOTAL_CLIENTS;
 constexpr size_t NUM_TOTAL_OPS = NUM_OPS_PER_CLIENT * TOTAL_CLIENTS;
@@ -82,7 +82,7 @@ constexpr bool SIMPLE_CAS_RELEASE_USE_CAS = false;
 
 // ─── Ticket FAA config ───
 
-constexpr size_t TICKET_FAA_ACTIVE_WINDOW = 16;
+constexpr size_t TICKET_FAA_ACTIVE_WINDOW = 8;
 constexpr bool TICKET_FAA_SHARD_OWNER = true;
 constexpr size_t TICKET_FAA_LOG_CAPACITY = TOTAL_CLIENTS * TICKET_FAA_ACTIVE_WINDOW * 4;
 constexpr size_t TICKET_FAA_CQ_BATCH = 32;
