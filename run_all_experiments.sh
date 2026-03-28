179b2429d0634e9b20edc272995dd0cbb7017d23
stevie9868 <yingjianwu2@email.com>
2026-03-27 22:38:03 -0700

    Parallelize node rebuilds in run_all_experiments.sh

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -27,10 +27,11 @@ for exp in "${EXPERIMENTS[@]}"; do
     git commit -m "$NAME: $NUM_OPS ops"
     git push
 
-    # Rebuild all nodes
+    # Rebuild all nodes in parallel
     for host in apt130 apt131 apt129 apt132 apt136; do
-        ssh stevie98@${host}.apt.emulab.net "cd /local/rdma && git checkout yingjianw/wip && git pull origin yingjianw/wip && cd build && make -j" > /dev/null 2>&1
+        ssh stevie98@${host}.apt.emulab.net "cd /local/rdma && git checkout yingjianw/wip && git pull origin yingjianw/wip && cd build && make -j" > /dev/null 2>&1 &
     done
+    wait  # Wait for all rebuilds to complete
 
     # Restart servers
     for i in 0 1 2 3 4; do
